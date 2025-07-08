702b215022b454a104f43a2c766a4ab69e602e5c
javieralvarezgrafico <javier.alvarez.grafico@gmail.com>
2025-07-08 13:01:51 -0400

    Arreglos en tarea 6, ya que tenia un poco de lio entre general.js y auth_users.js. Arreglos de codigo

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,35 +9,20 @@ public_users.post("/register", (req,res) => {
     let username = req.body.username;
     let password = req.body.password;
 
-    let users = []
-
-    // Check if a user with the given username already exists
-    const doesExist = (username) => {
-        // Filter the users array for any user with the same username
-        let userswithsamename = users.filter((user) => {
-            return user.username === username;
-        });
-        // Return true if any user with the same username is found, otherwise false
-        if (userswithsamename.length > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
-        if (!doesExist(username)) {
+        if (!isValid(username)) {
             // Add the new user to the users array
             users.push({"username": username, "password": password});
             return res.status(200).json({ message: "User " + username + " successfully registered. Now you can login" });
         } else {
             return res.status(404).json({message: "User already exists!"});
         }
+    } else {
+        // Return error if username or password is missing
+        return res.status(404).json({message: "Unable to register user."});
     }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
 });
 
 // TAREA 1. Get the book list available in the shop
