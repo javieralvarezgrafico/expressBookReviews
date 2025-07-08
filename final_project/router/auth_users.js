d0fd07a9542ebef5aa56904ecfffbac7280ce982
javieralvarezgrafico <javier.alvarez.grafico@gmail.com>
2025-07-08 15:45:07 -0400

    Tarea 9 parece que completada pero aun no hice la prueba en postman. Commit por descanso

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -55,8 +55,21 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const bookIsbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization?.username;
+    const bookRequested = books[bookIsbn];
+
+    if (!bookRequested) {
+        return res.status(404).json({ message: "Book not found." });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: "Review is required." });
+    }
+
+    bookRequested.reviews[username] = review;
+    return res.status(200).json({ message: "Review successfully added/updated." });
 });
 
 module.exports.authenticated = regd_users;
