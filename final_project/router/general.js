a186794d5399bab92ebb749a1da7a60346bc5bdc
javieralvarezgrafico <javier.alvarez.grafico@gmail.com>
2025-07-09 10:40:08 -0400

    Todas las tareas completadas :)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+// Añadido para Tareas que requieren Axios (tareas 10, 11, 12, 13)
+const axios = require('axios');
 
 // TAREA 6. Register new user
 public_users.post("/register", (req,res) => {
@@ -30,6 +32,16 @@ public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4));
 });
 
+// TAREA 10. CON PROMESAS O ASYNC-AWAIT
+axios.get("https://javieralvar4-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/")
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error("Error al obtener los libros:", error.message);
+    });
+
+
 // TAREA 2. Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const bookIsbn = req.params.isbn;
@@ -40,7 +52,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
         res.status(404).json({ message: "Book not found" });
     }
  });
-  
+
+// TAREA 11. CON PROMESAS O ASYNC-AWAIT
+axios.get("https://javieralvar4-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/3")
+ .then(response => {
+    console.log("Libro obtenido por ISBN:");
+    console.log(response.data);
+ })
+ .catch(error => {
+    console.error("Error al obtener los detalles del libro", error.message);
+ })
+
+ 
 // TAREA 3. Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const bookAuthor = req.params.author;
@@ -59,6 +82,16 @@ public_users.get('/author/:author',function (req, res) {
     }
 });
 
+// TAREA 12. CON PROMESAS O ASYNC-AWAIT
+axios.get("https://javieralvar4-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/Samuel Beckett")
+ .then(response => {
+    console.log("Libro obtenido por Autor:");
+    console.log(response.data);
+ })
+ .catch(error => {
+    console.error("Error al obtener los detalles del libro", error.message);
+ })
+
 // TAREA 4. Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const bookTitle = req.params.title;
@@ -76,6 +109,16 @@ public_users.get('/title/:title',function (req, res) {
         res.status(404).json({ message: "Title not found" });
     }
 });
+
+// TAREA 13. CON PROMESAS O ASYNC-AWAIT
+axios.get("https://javieralvar4-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/Fairy tales")
+ .then(response => {
+    console.log("Libro obtenido por Título:");
+    console.log(response.data);
+ })
+ .catch(error => {
+    console.error("Error al obtener los detalles del libro", error.message);
+ })
 
 // TAREA 5. Get book review
 public_users.get('/review/:isbn',function (req, res) {
